b1aab1aabcd87003612dd4ce54322c0222b70f5c
roll <roll@post.agency>
2016-02-28 22:54:37 +0300

    updated packaging

--- setup.py
+++ setup.py
@@ -2,51 +2,70 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
-from __future__ import unicode_literals
 
 import os
 import io
-import json
 from setuptools import setup, find_packages
 
 
 # Helpers
-def read(path):
+def read(*paths):
+    """Read a text file."""
     basedir = os.path.dirname(__file__)
-    return io.open(os.path.join(basedir, path), encoding='utf-8').read()
-def clean(deps):
-    res = []
-    for dep in deps:
-        if dep and not dep.startswith('#') and not dep.startswith('git'):
-            res.append(dep)
-    return res
+    fullpath = os.path.join(basedir, *paths)
+    contents = io.open(fullpath, encoding='utf-8').read().strip()
+    return contents
 
 
 # Prepare
-readme = read('README.md')
-license = read('LICENSE.txt')
-requirements = clean(read('requirements.txt').split())
-requirements_dev = clean(read('requirements.dev.txt').split())
-package = json.loads(read('package.json'))
+PACKAGE = 'mario'
+INSTALL_REQUIRES = [
+    'pyyaml',
+]
+LINT_REQUIRES = [
+    'pylint',
+]
+TESTS_REQUIRE = [
+    'tox',
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['examples', 'tests'])
 
 
 # Run
 setup(
-    name=package['name'],
-    version=package['version'],
-    description=package['description'],
-    long_description=readme,
-    author=package['author'],
-    author_email=package['author_email'],
-    url=package['repository'],
-    license=package['license'],
+    name=PACKAGE,
+    version=VERSION,
+    packages=PACKAGES,
     include_package_data=True,
-    packages=find_packages(exclude=['examples', 'tests']),
-    install_requires=requirements,
-    tests_require=requirements_dev,
-    test_suite='nose.collector',
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': LINT_REQUIRES + TESTS_REQUIRE},
     zip_safe=False,
-    keywords=package['keywords'],
-    classifiers=package['classifiers'],
-    entry_points={'console_scripts': package['console_scripts']},
+    long_description=README,
+    console_scripts=['mario = mario:cli'],
+    description='Mario is a radically simple task runner.',
+    author='roll',
+    author_email='roll@post.agency',
+    url='https://github.com/inventive-ninja/mario-py',
+    license='MIT',
+    keywords=[
+        'task runner',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
